3a347e1c7686ecae51d796a2888b70e823e62a28
Andreas Kring <andreas@magenta.dk>
2016-03-04 16:16:19 +0100

    Work on code to save extraction format settings begun

---
+++ src/main/java/dk/magenta/eark/erms/Constants.java
@@ -0,0 +1,14 @@
+package dk.magenta.eark.erms;
+
+public class Constants {
+
+	// Accepted extraction formats
+	public static final String EXTRACTION_FORMAT_EARKSIP = "earkSIP";
+	
+	// JSON keys
+	public static final String SUCCESS = "success";
+	public static final String ERRORMSG = "error";
+	
+	// File paths
+	public static final String PATH_SETTINGS = "/home/andreas/eark/export-erms/configuration";
+}
--- src/main/java/dk/magenta/eark/erms/MyResource.java
+++ src/main/java/dk/magenta/eark/erms/MyResource.java
@@ -1,8 +1,15 @@
 package dk.magenta.eark.erms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -11,6 +18,29 @@ import javax.ws.rs.core.MediaType;
 @Path("myresource")
 public class MyResource {
 
+	private List<String> acceptedExtractionFormats;
+	
+	public MyResource() {
+		acceptedExtractionFormats = new ArrayList<String>();
+		acceptedExtractionFormats.add(Constants.EXTRACTION_FORMAT_EARKSIP);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("save-extraction-format")
+	public JsonObject saveExtractionFormat(@QueryParam("format") String format) {
+		
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		if (acceptedExtractionFormats.contains(format)) {
+			builder.add(Constants.SUCCESS, true);
+		} else {
+			builder.add(Constants.SUCCESS, false);
+			builder.add(Constants.ERRORMSG, "Invalid extraction format");
+		}
+
+		return builder.build();
+	}
+	
     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
@@ -23,8 +53,14 @@ public class MyResource {
         return "Got it YES sir hut hurra...Ihh jep igen kkjubiiiii kk nnnn mmmm!";
     }
     
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public JsonObject 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("test")
+    public JsonObject test() {
+    	JsonObject json = Json.createObjectBuilder()
+    			.add("success", false)
+    			.build();
+    	return json;
+    }
     
 }
